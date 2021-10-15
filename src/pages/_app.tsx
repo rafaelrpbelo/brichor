1bb8c3b2a19cc19940c7f7afd4ea952219d25518
Rafael Belo <rafaelrpbelo@gmail.com>
2021-10-15 09:09:28 -0300

    Create Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import Head from "next/head"
 
+import { Header } from "../components/Header";
+
 import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>brichor</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   )
