e53115d485197f2847c0ec29aa1ae755a9b5f34d
Rafael Belo <rafaelrpbelo@gmail.com>
2021-10-15 08:43:11 -0300

    Add title to the main component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from "next/app";
+import Head from "next/head"
+
+import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>brichor</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
