b2c3b30e45bfafb6cd714c2039d5c59b093508b3
Rafael Belo <rafaelrpbelo@gmail.com>
2021-10-17 21:31:01 -0300

    Store signed in users in FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,44 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
+import { query as q } from "faunadb"
+
+import { fauna } from "../../../services/fauna"
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: "read:user",
-      //redirect_uri: "http://localhost:3000/api/auth"
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(
+              q.Index("users_by_email"),
+              q.Casefold(user.email)
+            ))),
+            q.Create(q.Collection("users"),
+              { data: { email } }
+            ),
+            q.Get(q.Match(
+              q.Index("users_by_email"),
+              q.Casefold(user.email)
+            ))
+          )
+        )
+
+        return true
+      } catch(e) {
+        console.log("cannot signin", e)
+        return false
+      }
+    },
+  },
 })
