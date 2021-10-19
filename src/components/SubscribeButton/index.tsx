347ed62b92ed6e48d2e8ffeada578f1286e4e8e3
Rafael Belo <rafaelrpbelo@gmail.com>
2021-10-18 23:21:26 -0300

    Enable user to create a subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,48 @@
+import { signIn, useSession } from "next-auth/client";
+
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss"
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton(props: SubscribeButtonProps) {
+interface SubscriptionResponseData {
+  data: {
+    sessionId: string;
+  }
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github")
+      return;
+    }
+
+    try {
+      const response: SubscriptionResponseData = await api.post("/subscribe")
+
+      const { sessionId } = response.data;
+
+      const stripeJs = await getStripeJs()
+
+      await stripeJs.redirectToCheckout({ sessionId })
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
